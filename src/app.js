578bdca9ebdd489ad7bc4213d6417feb013d900f
Lucas Solari <solarilucas92@gmail.com>
2023-05-26 17:01:40 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import generateRandomCard from "../components/random-Card-Generator";
+import generateRandomCard from "../components/random-Card-Generator.js";
 
 const app = document.querySelector("#app");
 const buttonShuffler = document.querySelector("#buttonRender");
